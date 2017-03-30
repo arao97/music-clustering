1d5dad775a268dab994e29be4586b965fbde0752
Akshay Rao <akshayrao97@gmail.com>
2017-03-30 19:51:15 +0530

    Changed to BIRCH clustering. Ignore filename.

--- src/song_dbscan.py
+++ src/song_dbscan.py
@@ -2,7 +2,9 @@
 
 from sklearn.cluster import DBSCAN
 from sklearn.cluster import KMeans
+from sklearn.cluster import Birch
 import sys
+import seaborn as sns
 import pandas as pd
 
 main_dir = sys.argv[1]
@@ -11,9 +13,10 @@ song_data = pd.read_csv(main_dir + 'song_pca.csv', encoding = 'latin-1')
 
 song_data.drop('track_id', axis = 1, inplace = True)
 
-print(song_data.values)
+'''sns.regplot('loudness','tempo',song_data)
+sns.plt.show()'''
 
-db = DBSCAN(eps = 10, min_samples = 1500, metric = 'euclidean').fit(song_data)
+'''db = DBSCAN(eps = 2, min_samples = 1500, metric = 'euclidean').fit(song_data)
 labels = db.labels_
 
 n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
@@ -24,3 +27,11 @@ print(n_clusters)
 kmeans = KMeans(n_clusters = 10).fit(song_data)
 
 print(kmeans.cluster_centers_)
+print(kmeans.labels_)'''
+
+brch = Birch(n_clusters = 9, compute_labels = True)
+brch.fit(song_data)
+print(brch.labels_)
+     
+
+     
